7d98d9e202349c05c0a3ee9ce475b42918d2745a
Gunther Schneider <gunthers@gmail.com>
2019-01-23 11:05:42 -0500

    Removed console log on initial require

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 "use strict";
 
-let def = require("./definitions/20160901");
-
-Object.keys(def).filter(k => k.length === 6).forEach(k => console.log(`{ "code": "${k}", "name": "${def[k].name}" },`.replace(/(?:\r\n|\r|\n)/g, ' ')));
-
 module.exports = require("./dist/GICS");
